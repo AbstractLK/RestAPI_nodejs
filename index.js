d1f9dcc97c8f16625b247712e894bd8637b00990
Kaveesha <kalshandhanasekara@gmail.com>
2023-07-17 13:33:30 +0530

    some update

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
-const mongoose = require('mongoose');
 const cors = require('cors');
 require('dotenv').config();
+require('./config/database').connect();
 
 const authRoute = require('./router/auth');
 const userRoute = require('./router/users');
@@ -9,15 +9,6 @@ const userRoute = require('./router/users');
 const app = express();
 app.use(cors());
 app.use(express.json());
-
-//database connection
-const database = mongoose.connect(process.env.DATABASE_URL,{});
-database.then(res => {
-    console.log('Database connected');
-});
-database.catch(error => {
-    console.log('Database error');
-});
 
 app.use(cors({
     origin: 'http://127.0.0.1:3000' // Set the allowed origin(s) for CORS
@@ -27,6 +18,6 @@ app.use(cors({
 app.use('/auth', authRoute);
 app.use('/user', userRoute);
 
-app.listen(3001,()=>{
+app.listen(process.env.APP_PORT,()=>{
     console.log(process.env.APP_NAME + ' started at port 3001');
 })
