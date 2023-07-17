2020f80b6af2a51aa731b615a6b9c4063b50ead2
Kaveesha <kalshandhanasekara@gmail.com>
2023-07-17 13:32:42 +0530

    generate access token

--- controller/user/user.registration.js
+++ controller/user/user.registration.js
@@ -1,26 +1,36 @@
-const userModel = require("../../models/User");
 const userDB = require('../../data/user.db');
+const jwt = require('jsonwebtoken');
 
-async function registerUser(req, res, next) {
+async function registerUser(req, res) {
     const data = req.body;
-
     const isExist = await userDB.isExist(data.contact);
     if (isExist) {
         console.log('Already registered');
-        res.send(isExist);
+        res.send(!isExist);
     }else {
-
         try {
-            await userDB.insert(data);
-            res.status(200).send(isExist);
+            const savedUser = await userDB.insert(data);
+            const accessToken = await generateAccessToken(savedUser._id, data.name, data.contact, '2h');
+            res.status(200).json({accessToken: accessToken, name: data.name});
             console.log('User created successfully');
         } catch (error) {
             console.log(error+' ');
-            res.status(400).json({message: 'user registrsation failed !'});
+            res.status(400).json({message: 'user registration failed !'});
         }
     }
-
-
 }
 
-module.exports = registerUser;
+
+function generateAccessToken(userId, name, contact, expireAt) {
+    return jwt.sign(
+        {id: userId, name: name, contact: contact},
+        process.env.JWT_SECURITY_KEY,
+        {expiresIn: expireAt}
+    );
+}
+
+let control = {
+    registerUser,
+    generateAccessToken
+}
+module.exports = control;
