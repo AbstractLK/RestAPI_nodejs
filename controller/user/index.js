5d60927b63e4be6a1337d5ed33c7d885ff8596be
Kaveesha <kalshandhanasekara@gmail.com>
2023-07-20 23:08:17 +0530

    add login function path

--- controller/user/index.js
+++ controller/user/index.js
@@ -2,10 +2,12 @@ const getAll = require('./user.getAll');
 const registration = require('./user.registration');
 const userDelete = require('./user.delete');
 const update = require('./user.update');
+const login = require('./user.login');
 
 module.exports = {
     getAll,
     registration,
     userDelete,
-    update
+    update,
+    login
 }
